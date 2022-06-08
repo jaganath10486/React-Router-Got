8c5e8673911a0377a2898018e86c11b37e62a77c
jaganath10486 <jaganathsegi104@gmail.com>
2022-06-08 10:45:48 +0530

    minor change

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,13 +4,13 @@ import {Link} from 'react-router-dom'
 function Card(props) {
   return (
     <React.Fragment>
-        <div className='col-md-auto' style={{paddingTop : '30px'}}>
-          <div className="card">
+        <div className='col-md-auto' style={{marginTop : '30px'}}>
+          <div className="card" style={{width : '300px'}}>
              <Link to={`character/${props.id}`}>
                 <img src={props.imageUrl} className="card-img-top" style={{height : '300px', width : '300px'}} />
              </Link>
              <div className="card-body" style={{textAlign : 'center'}}>
-              <h5 className="card-title">{props.name}</h5>
+              <h5 className="card-title" style={{color : 'red'}}>{props.name}</h5>
              </div>
           </div>
         </div>
